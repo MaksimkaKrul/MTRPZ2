04b2016b0747cb9809731d94e9d789282a763bd3
MaksimkaKrul <krulevckiym@gmail.com>
2025-05-21 00:30:55 +0300

    Finished ArrayList: added deleteAll, get, clone, reverse, findFirst, findLast, clear, extend

--- array_list.py
+++ array_list.py
@@ -17,3 +17,37 @@ class ArrayList:
         if index < 0 or index >= len(self.data):
             raise IndexError("Index out of bounds")
         return self.data.pop(index)
+
+    def deleteAll(self, element):
+        self.data = [e for e in self.data if e != element]
+
+    def get(self, index):
+        if index < 0 or index >= len(self.data):
+            raise IndexError("Index out of bounds")
+        return self.data[index]
+
+    def clone(self):
+        new_list = ArrayList()
+        new_list.data = self.data.copy()
+        return new_list
+
+    def reverse(self):
+        self.data.reverse()
+
+    def findFirst(self, element):
+        try:
+            return self.data.index(element)
+        except ValueError:
+            return -1
+
+    def findLast(self, element):
+        for i in reversed(range(len(self.data))):
+            if self.data[i] == element:
+                return i
+        return -1
+
+    def clear(self):
+        self.data.clear()
+
+    def extend(self, elements):
+        self.data.extend(elements.data)
